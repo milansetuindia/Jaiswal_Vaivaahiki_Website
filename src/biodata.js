fa54fec9c9f9d6050603487650aa108db9623351
milansetuindia <milansetuindia@gmail.com>
2026-08-19 16:46:15 +0530

    Add files via upload

---
+++ src/biodata.js
@@ -0,0 +1,31 @@
+const biodata = [
+  {
+    id: 1,
+    name: "Rahul Kumar",
+    age: 27,
+    city: "Patna",
+    education: "B.Tech",
+    occupation: "Software Engineer",
+    image: "https://randomuser.me/api/portraits/men/45.jpg",
+  },
+  {
+    id: 2,
+    name: "Priya Kumari",
+    age: 24,
+    city: "Ranchi",
+    education: "MBA",
+    occupation: "Teacher",
+    image: "https://randomuser.me/api/portraits/women/65.jpg",
+  },
+  {
+    id: 3,
+    name: "Amit Kumar",
+    age: 29,
+    city: "Varanasi",
+    education: "M.Tech",
+    occupation: "Data Scientist",
+    image: "https://randomuser.me/api/portraits/men/75.jpg",
+  },
+];
+
+export default biodata;
