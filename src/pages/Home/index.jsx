d25b163892d66c30316ceb74b1973c6756018f58
milansetuindia <milansetuindia@gmail.com>
2026-08-22 22:59:47 +0530

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,33 @@
+// src/pages/Home/index.jsx
+
 import MainLayout from "../../components/layout/MainLayout";
 
 import Hero from "../../components/home/Hero";
+import TrustStats from "../../components/home/TrustStats";
 import SearchSection from "../../components/home/SearchSection";
 import LatestProfiles from "../../components/home/LatestProfiles";
 import Features from "../../components/home/Features";
+import HowItWorks from "../../components/home/HowItWorks";
+import HomeCTA from "../../components/home/HomeCTA";
 
 function Home() {
   return (
     <MainLayout>
+
       <Hero />
+
+      <TrustStats />
+
       <SearchSection />
+
       <LatestProfiles />
+
       <Features />
+
+      <HowItWorks />
+
+      <HomeCTA />
+
     </MainLayout>
   );
 }
